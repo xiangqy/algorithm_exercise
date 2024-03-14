a2b1f35f5c72041a0c7b3a7d6546329c2810a29b
naby <xiangqy0201@yeah.net>
2024-03-14 15:50:20 +0800

    增加冒泡排序

--- common/log/log_setting.h
+++ common/log/log_setting.h
@@ -35,10 +35,11 @@
     }                                                            \
   } while (0)
 
-#include <spdlog/sinks/rotating_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/async.h>
-#include <spdlog/spdlog.h>
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/async.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/fmt/ranges.h"
 
 class log_setting {
  public:
--- sort/bubbling/bubbling.cc
+++ sort/bubbling/bubbling.cc
@@ -1,6 +1,20 @@
 #include "log/log_setting.h"
 
+void bubbling(std::vector<int>& sort_data) {
+  if (sort_data.size() < 2) return;
+  for (auto i = 0; i < sort_data.size() - 1; ++i) {
+    for (auto j = i + 1; j < sort_data.size(); ++j) {
+      if (sort_data[i] > sort_data[j]) {
+        std::swap(sort_data[i], sort_data[j]);
+      }
+    }
+  }
+}
+
 int main(int argc, char** argv) {
-  LogInfo("This is info message.");
+  std::vector<int> sort_data = {4, 1, 2, 3};
+  LogInfo("Before sort {}", sort_data);
+  bubbling(sort_data);
+  LogInfo("After sorted {}", sort_data);
   return 0;
 }
